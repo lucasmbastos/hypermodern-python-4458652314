377b4e0fc5d6dcf0040f23370b8bd4a374f3d59e
Lucas de Miranda <lucasmbastos94@gmail.com>
2020-03-07 19:12:41 -0300

    Add support to multilanguage from wikipedia

--- src/hypermodern_python_lucasmbastos/wikipedia.py
+++ src/hypermodern_python_lucasmbastos/wikipedia.py
@@ -1,8 +1,15 @@
 import requests
+import click
 
-API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
+API_URL = "https://{}.wikipedia.org/api/rest_v1/page/random/summary"
 
-def random_page():
-    with requests.get(API_URL) as response:
-        response.raise_for_status()
-        return response.json()
+def random_page(language="en"):
+    url = API_URL.format(language)
+
+    try:
+        with requests.get(url) as response:
+            response.raise_for_status()
+            return response.json()
+    except requests.RequestException as error:
+        message = str(error)
+        raise click.ClickException(message)
--- tests/test_console.py
+++ tests/test_console.py
@@ -1,20 +1,16 @@
 import click.testing
 import pytest
+import requests
 
 from hypermodern_python_lucasmbastos import console
 
 @pytest.fixture
-def mock_requests_get(mocker):
-	mock = mocker.patch("requests.get")
-	mock.return_value.__enter__.return_value.json.return_value = {
-		"title": "Lorem Ipsum",
-		"extract": "Lorem ipsum dolor sit amet"
-	}
-	return mock
-
-@pytest.fixture
 def runner():
 	return click.testing.CliRunner()
+
+@pytest.fixture
+def mock_wikipedia_random_page(mocker):
+	return mocker.patch("hypermodern_python_lucasmbastos.wikipedia.random_page")
 
 def test_main_succeeds(runner, mock_requests_get):
 	result = runner.invoke(console.main)
@@ -37,3 +33,12 @@ def test_main_fails_on_request_error(runner, mock_requests_get):
 	mock_requests_get.side_effect = Exception("Boom")
 	result = runner.invoke(console.main)
 	assert result.exit_code == 1
+
+def test_main_prints_message_on_request_error(runner, mock_requests_get):
+	mock_requests_get.side_effect = requests.RequestException
+	result = runner.invoke(console.main)
+	assert "Error" in result.output
+
+def test_main_uses_specified_language(runner, mock_wikipedia_random_page):
+	runner.invoke(console.main, ["--language=pl"])
+	mock_wikipedia_random_page.assert_called_with(language="pl")
