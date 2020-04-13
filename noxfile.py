38e68f1e92825b173621915fcd6093bdb64a5c45
Lucas de Miranda <lucasmbastos94@gmail.com>
2020-04-13 12:49:52 -0300

    Add support to version management of nox libs with poetry

--- noxfile.py
+++ noxfile.py
@@ -6,10 +6,29 @@ import nox
 nox.options.sessions = "lint", "safety", "tests"
 
 
+# This allow us to use poetry to manage versions of nox libs.
+# We use the constraints feature of poetry to lock versions of our libs
+def install_with_constraints(session, *args, **kwargs):
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--dev",
+            "--format=requirements.txt",
+            f"--output={requirements.name}",
+            external=True,
+        )
+
+        session.install(f"--constraint={requirements.name}", *args, **kwargs)
+
+
 @nox.session(python=["3.8", "3.7"])
 def tests(session):
     args = session.posargs or ["--cov", "-m", "not e2e"]
-    session.run("poetry", "install", external=True)
+    session.run("poetry", "install", "--no-dev", external=True)
+    install_with_constraints(
+        session, "coverage[toml]", "pytest", "pytest-cov", "pytest-mock"
+    )
     session.run("pytest", *args)
 
 
@@ -19,7 +38,8 @@ locations = "src", "tests", "noxfile.py"
 @nox.session(python=["3.8", "3.7"])
 def lint(session):
     args = session.posargs or locations
-    session.install(
+    install_with_constraints(
+        session,
         "flake8",
         "flake8-bandit",
         "flake8-black",
@@ -32,7 +52,7 @@ def lint(session):
 @nox.session(python="3.8")
 def black(session):
     args = session.posargs or locations
-    session.install("black")
+    install_with_constraints(session, "black")
     session.run("black", *args)
 
 
@@ -48,5 +68,5 @@ def safety(session):
             f"--output={requirements.name}",
             external=True,
         )
-        session.install("safety")
+        install_with_constraints(session, "safety")
         session.run("safety", "check", f"--file={requirements.name}", "--full-report")
