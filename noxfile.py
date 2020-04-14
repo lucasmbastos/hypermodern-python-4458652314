a234fa9d2d9353ee85218704bf036e2c48c518d4
Lucas de Miranda <lucasmbastos94@gmail.com>
2020-04-14 11:18:18 -0300

    Add pytype dependency

--- noxfile.py
+++ noxfile.py
@@ -3,7 +3,7 @@ import tempfile
 
 import nox
 
-nox.options.sessions = "lint", "mypy", "safety", "tests"
+nox.options.sessions = "lint", "mypy", "pytype", "safety", "tests"
 
 
 # This allow us to use poetry to manage versions of nox libs.
@@ -77,3 +77,11 @@ def mypy(session) -> None:
     args = session.posargs or locations
     install_with_constraints(session, "mypy")
     session.run("mypy", *args)
+
+
+@nox.session(python=["3.7"])
+def pytype(session):
+    """Run the static type checker."""
+    args = session.posargs or ["--disable=import-error", *locations]
+    install_with_constraints(session, "pytype")
+    session.run("pytype", *args)
